9b1da234a2c509cd014c008c7d607d2ffad165d3
A. Slyadz <torin-st@yandex.ru>
2021-04-15 07:53:50 +0300

    initial commit

---
+++ src/main/java/com/slyadz/misy2/Misy2Application.java
@@ -0,0 +1,13 @@
+package com.slyadz.misy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Misy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Misy2Application.class, args);
+	}
+
+}
