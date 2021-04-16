4fc3eac8da5d7073446e80cde1d3b765d444629a
A. Slyadz <torin-st@yandex.ru>
2021-04-16 09:18:16 +0300

    add new module misy2-users-service

---
+++ misy2-users-service/src/main/java/com/slyadz/misy2/userssevice/Misy2UsersSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.slyadz.misy2.userssevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Misy2UsersSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Misy2UsersSeviceApplication.class, args);
+	}
+
+}
---
+++ misy2-users-service/src/test/java/com/slyadz/misy2/userssevice/Misy2UsersSeviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.slyadz.misy2.userssevice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Misy2UsersSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
