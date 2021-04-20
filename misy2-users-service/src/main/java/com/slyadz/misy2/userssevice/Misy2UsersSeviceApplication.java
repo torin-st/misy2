152c4898cdf939195a798e982f724a3997fb7f1d
A. Slyadz <torin-st@yandex.ru>
2021-04-20 05:35:26 +0300

    add db demo data during startup

--- misy2-users-service/src/main/java/com/slyadz/misy2/userssevice/Misy2UsersSeviceApplication.java
+++ misy2-users-service/src/main/java/com/slyadz/misy2/userssevice/Misy2UsersSeviceApplication.java
@@ -1,13 +1,28 @@
 package com.slyadz.misy2.userssevice;
 
+import com.slyadz.misy2.userssevice.model.User;
+import com.slyadz.misy2.userssevice.repository.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Misy2UsersSeviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Misy2UsersSeviceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(UserRepository userRepository) {
+		return args -> {
+			userRepository.save(new User("Jake"));
+			userRepository.save(new User("Jane"));
+			userRepository.save(new User("John"));
+			userRepository.save(new User("Jennifer"));
+		};
 	}
 
 }
