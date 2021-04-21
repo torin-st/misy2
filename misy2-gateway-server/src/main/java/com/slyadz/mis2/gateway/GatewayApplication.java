72b54ac2bc4520662961a79b05d2ec5aebc9a55d
A. Slyadz <torin-st@yandex.ru>
2021-04-21 08:56:43 +0300

    add new module misy2-gateway server

---
+++ misy2-gateway-server/src/main/java/com/slyadz/mis2/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.slyadz.mis2.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
+}
