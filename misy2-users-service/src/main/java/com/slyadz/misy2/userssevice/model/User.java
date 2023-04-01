62950ecadc8d290f196e61c6328dea28b415bd33
Andrey Slyadz <torin-st@yandex.ru>
2023-04-01 12:27:26 +0300

    Bump Spring Boot

--- misy2-users-service/src/main/java/com/slyadz/misy2/userssevice/model/User.java
+++ misy2-users-service/src/main/java/com/slyadz/misy2/userssevice/model/User.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 @Entity
+@Table(name = "users")
 public class User {
 		public interface Views {
 		interface idName {}
